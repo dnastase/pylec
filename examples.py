5fa6bbee4a1132a39329db705e25c5d5534b1bf2
Daniel <dan@thinkpad>
2014-01-28 08:06:26 -0500

    examples

---
+++ examples.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+n=2
+f=3.14
+s="string"
+n1 = n + 1
+s1 = s + "another"
+
+print(n, f s)
+
+if n > 2: print ">2" 
+elif n < 2: print "<2"
+else: print "=2"
+
+l = [n, f, s]
+l[1]
+
+for e in l: print e
+
+
